84a8cc268c1baa31180d313f46e94fb6e60ad6aa
IMedrai <63478457+IMedrai@users.noreply.github.com>
2020-04-12 14:23:51 +0100

    Ajout attrs+propr+cons(classe produit)

--- Pharmacie/Produit.cs
+++ Pharmacie/Produit.cs
@@ -4,12 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace Pharmacie
 {
-    class Produit
-    {
-        private String refProduit;
-        private String libelleProduit;
-        private Double prixProduit;
-    }
+	class Produit
+	{
+		private string refProduit;
+		private int codeBar;
+		private double prix;
+		private string libelle;
+		
+
+
+		public string RefProduit
+		{
+			get { return refProduit; }
+			set { refProduit = value; }
+		}
+
+		public int CodeBar
+		{
+			get { return codeBar; }
+			set { codeBar = value; }
+		}
+
+		public double Prix
+		{
+			get { return prix; }
+			set { prix = value; }
+		}
+
+		public string Libelle
+		{
+			get { return libelle; }
+			set { libelle = value; }
+		}
+
+		private Produit()
+		{
+		}
+
+		private Produit(string refProduit, int codeBar, double prix, string libelle)
+		{
+			this.refProduit = refProduit;
+			this.codeBar = codeBar;
+			this.prix = prix;
+			this.libelle = libelle;
+		}
+
+
+	}
 }
+
+
